664ccd2c22d0a9d514f1137ed9d5c1ba1df3e70c
Erathaowl <46474708+erathaowl@users.noreply.github.com>
2024-10-11 17:23:34 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+GITURL=https://github.com/erathaowl/configs/blob/main
+
+# install stuffs
+apt update && apt install -y \
+  net-tools \
+  git \
+  curl \
+  nmap \
+  wget \
+  bat \
+  starship \
+  tmux
+
+# Download config files
+wget --force-directories $GITURL/.ssh/config -o ~/.ssh/config
+wget --force-directories $GITURL/.config/starship.toml -o ~/.config/starship.toml
+wget $GITURL/tmux/lite/.tmux.conf -o ~/.tmux.conf
+wget $GITURL/bash/.bashrc -o ~/.bash_addons
+wget $GITURL/bash/.bash_aliases -o ~/.bash_aliases
+
+# add bash_addons loading line
+if grep -Fxq ".bash_addons" ~/.bashrc
+then
+    :
+else
+    echo ". ~/bash_addons" >> ~/.bashrc
+fi
+

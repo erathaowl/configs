8b25c29199fb82108f9c501ae6e0fc122454c539
Erathaowl <erathaowl@gmail.com>
2026-03-04 10:50:20 +0100

    updated powershell profile script

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-# C:\Users\{user}\Documents\PowerShell
+# C:\Users\<username>\Documents\PowerShell
 
 function uvr {
     uv run @args
@@ -11,3 +11,32 @@ function uvp {
 function uvm {
     uv run python manage.py @args
 }
+
+# python3XX run the corrisponding interpretere launching py -3.xx
+function python {
+    param(
+        [Parameter(Position = 0, ValueFromRemainingArguments = $true)]
+        [string[]]$Args
+    )
+
+    $invokedAs = $MyInvocation.InvocationName
+
+    # python3  -> lancia l'ultima 3.x disponibile
+    if ($invokedAs -eq 'python3') {
+        & py -3 @Args
+        return
+    }
+
+    # python3XX -> lancia 3.XX
+    if ($invokedAs -match '^python3(\d{1,2})$') {
+        & py "-3.$($matches[1])" @Args
+        return
+    }
+
+    # fallback
+    & py @Args
+}
+
+# Alias python3 (latest) + python3XX (specific)
+Set-Alias -Name python3 -Value python -Scope Global
+3..20 | ForEach-Object { Set-Alias -Name "python3$_" -Value python -Scope Global }
